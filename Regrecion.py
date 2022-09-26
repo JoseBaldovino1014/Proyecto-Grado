da4c9ee12cd345002b5c6b8d25a1c55ce2eb8518
francisco <fjsandovala@unac.edu.co>
2022-09-26 10:38:16 -0500

    values

--- Regrecion.py
+++ Regrecion.py
@@ -6,9 +6,9 @@ import numpy.random as nr
 import math
 
 # load datasets
-custs= pd.read_csv('./data/AdvWorksCusts.csv')
-spend_Month = pd.read_csv('./data/AW_AveMonthSpend.csv')
-buyer_bike = pd.read_csv('./data/AW_BikeBuyer.csv')
+custs= pd.read_csv('./BD/AdvWorksCusts.csv')
+spend_Month = pd.read_csv('./BD/AW_AveMonthSpend.csv')
+buyer_bike = pd.read_csv('./BD/AW_BikeBuyer.csv')
 
 # remove duplicates AdvWorksCusts.csv
 
@@ -33,6 +33,6 @@ print(buyer_bike.shape)
 print(buyer_bike.CustomerID.unique().shape)
 
 # check for missing values
-print(buyer_bike.astype(np.object).isnull()).any()
-print(spend_Month.astype(np.object).isnull()).any()
-print(custs.astype(np.object).isnull()).any()
+print(buyer_bike.astype(object).isnull().any())
+print(spend_Month.astype(object).isnull().any())
+print(custs.astype(np.object).isnull().any())
